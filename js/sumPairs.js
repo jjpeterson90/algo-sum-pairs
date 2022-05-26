e2e682b18852dbd86bf6203ff99c8f107fadd859
Justin <jjpeterson90@hotmail.com>
2022-05-26 14:50:35 -0500

    complete

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,12 @@
 exports.sumPairs = function() {
-
+    let result = [];
+    for (let i = 0; i < Array.length - 1; i++) {
+        for (let j = i + 1; j < Array.length; j++) {
+            if (arr[i] + arr[j] === num) {
+                result.push([arr[i], arr[j]]);
+            }
+        }
+    }
+    if (result.length === 0) return 'Unable to find pairs.'
+    else return result;
 };
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,7 @@
 var sp = require("./sumPairs");
 
+console.log(sp.sumPairs([1,2,3,4,5], 9))
+console.log(sp.sumPairs([1,2,3,4,5], 7))
+console.log(sp.sumPairs([3,1,5,8,2], 27))
+
 // Don't forget to add your tests :)
